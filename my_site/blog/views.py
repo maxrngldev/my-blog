27219694046fe83b3aefe723b6f1f0c0c6d694a9
maxrngldev <maxrngl08@gmail.com>
2023-04-14 16:22:49 -0600

    finished comments section

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from .models import Post
 from .forms import CommentForm
@@ -29,12 +32,35 @@ class AllPostsView(ListView):
     context_object_name = 'all_posts'
 
 
-class SinglePostView(DetailView):
+class SinglePostView(View):
     template_name = 'blog/post-detail.html'
     model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post_tags'] = self.object.tags.all()
-        context['comment_form'] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'post_comments': post.comments.all().order_by('-id'),
+            'comment_form': CommentForm
+        }
+        return render(request, 'blog/post-detail.html', context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse('post-detail-page', args=[slug]))
+
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'post_comments': post.comments.all().order_by('-id'),
+            'comment_form': comment_form
+        }
+        return render(request, 'blog/post-detail.html', context)
